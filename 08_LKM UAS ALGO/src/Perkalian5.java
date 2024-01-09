705f83ae03887aed9a91bdffb0c48c0d3c8aec91
intaniamonafatana <146052216+intaniamonafatana@users.noreply.github.com>
2024-01-09 11:34:48 +0700

    09/01/2024

---
+++ 08_LKM UAS ALGO/src/Perkalian5.java
@@ -0,0 +1,11 @@
+import  java.util.Scanner;
+
+public class Perkalian5 {
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++) {
+            int hasil = i * 5;
+            System.out.println(i + " * 5 = " + hasil);
+        }
+    }
+}
